b8d636a5fe4fc9559d0a76e66ce4e2b24a153560
legobt <6wbvkn0j@anonaddy.me>
2024-06-19 21:59:50 +0000

    feat: allow overriding default Transform stream constructor options

--- src/util.js
+++ src/util.js
@@ -1,16 +1,19 @@
 const { Transform, pipeline } = require('readable-stream')
 const pify = require('pify')
 
-const TRANSFORM_OPTIONS = {
+const DEFAULT_TRANSFORM_OPTIONS = {
   highWaterMark: 16,
   objectMode: true
 }
 
 class ModuleGroup {
-  constructor ({ label, parentGroup }) {
+  constructor ({ label, parentGroup, ...streamOptions }) {
     this.label = label
     this.parentGroup = parentGroup
-    this.stream = new Transform(TRANSFORM_OPTIONS)
+    this.stream = new Transform({
+      ...DEFAULT_TRANSFORM_OPTIONS,
+      ...streamOptions
+    })
   }
 }
 
@@ -18,21 +21,25 @@ module.exports = { createForEachStream, getStreamResults, ModuleGroup }
 
 const noop = () => {}
 
-function createForEachStream ({ onEach = noop, onEnd = noop }) {
+function createForEachStream ({ onEach = noop, onEnd = noop, ...streamOptions }) {
   return new Transform({
-    ...TRANSFORM_OPTIONS,
+    ...DEFAULT_TRANSFORM_OPTIONS,
     transform: (entry, _, cb) => { onEach(entry); cb() },
-    final: (cb) => { onEnd(); cb() }
+    final: (cb) => { onEnd(); cb() },
+    ...streamOptions
   })
 }
 
-async function getStreamResults (stream) {
+async function getStreamResults (stream, streamOptions) {
   // get bundle results
   const results = []
   await pify(cb => {
     pipeline(
       stream,
-      createForEachStream({ onEach: (entry) => { results.push(entry) } }),
+      createForEachStream({
+        onEach: (entry) => { results.push(entry) },
+        ...streamOptions
+      }),
       cb
     )
   })()
